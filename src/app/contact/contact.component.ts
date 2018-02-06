ef4aaf9df5748b679ed3a2ba055786e90b6aa48c
unknown <luka1cosic67@gmail.com>
2018-02-06 09:41:23 +0100

    Validation added code(still not working)

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -19,7 +19,55 @@ export class ContactComponent implements OnInit {
 }
 
   ngOnInit() {
+  /*  $(function () {
+                                                            $(".form-contact").validate({
+                                                                highlight: function (element) {
+                                                                    $(element).closest('.form-group').addClass("has-danger");
+                                                                    $(element).addClass("form-control-danger");
+                                                                },
+                                                                unhighlight: function (element) {
+                                                                    $(element).closest('.form-group').removeClass('has-danger').addClass('has-success');
+                                                                    $(element).removeClass('form-control-danger').addClass('form-control-success');
+                                                                },
+                                                                rules: {
+                                                                    name: {
+                                                                        required: true,
+                                                                        rangelength: [3, 255]
+                                                                    },
+                                                                    email: {
+                                                                        required: true,
+                                                                        rangelength: [5, 255],
+                                                                        email: true
+                                                                    },
+                                                                    message: {
+                                                                        required: true,
+                                                                        rangelength: [10, 255]
+                                                                    }
 
+                                                                },
+                                                                messages: {
+                                                                    name: {
+                                                                        required: "The *Name field is required!",
+                                                                        rangelength: "The *Name must be between 3 and 255 characters!"
+                                                                    },
+                                                                    email: {
+                                                                        required: "The *Email field is required!",
+                                                                        rangelength: "The *Email must be between 5 and 255 characters!",
+                                                                        email: "Please enter a valid email addres!"
+                                                                    },
+                                                                    message: {
+                                                                        required: "The *Message field is required!",
+                                                                        rangelength: "The *Message must be between 10 and 255 characters!",
+                                                                        email: "Please enter a valid message!"
+                                                                    }
+                                                                },
+                                                                errorElement: 'p',
+                                                                errorPlacement: function (error, element) {
+                                                                    error.appendTo($(element).closest('.form-group').find('.error'));
+                                                                }
+
+                                                            });
+                                                        }); */
 }
 
 
